16bf68f71efd9d3d8d6bd48544affba6aea67ca3
rav-andre <aoravagnani@gmail.com>
2022-08-01 16:30:48 -0300

    Add files 1

---
+++ contracts/Registrable.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <=0.9.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Registrable is Ownable {
+  mapping(address => bool) public allowedUsers;
+
+  function newAllowedUser(address allowed) public onlyOwner {
+    allowedUsers[allowed] = true;
+  }
+
+  modifier mustBeAllowedUser() {
+    require(allowedUsers[msg.sender], "Not allowed user");
+    _;
+  }
+}
