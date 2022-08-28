98346b8c0153e8e114ad7220be3181e3ba5119b2
Everson Silva <eversonsilva9799@gmail.com>
2022-08-28 16:57:48 -0300

    refact developer pool

--- contracts/DeveloperPool.sol
+++ contracts/DeveloperPool.sol
@@ -17,9 +17,9 @@ import "./Callable.sol";
 contract DeveloperPool is Ownable, Blockable, Callable, PoolInterface {
   using SafeMath for uint256;
 
-  uint256 constant public FIXED_POINT = 10**18;
-  uint256 constant public TOKENS_PER_ERA = 833333000000000000000000;
-  uint256 constant public ERAS = 18;
+  uint256 public constant FIXED_POINT = 10**18;
+  uint256 public constant TOKENS_PER_ERA = 833333000000000000000000;
+  uint256 public constant ERAS = 18;
 
   SacTokenInterface internal sacToken;
 
@@ -33,23 +33,25 @@ contract DeveloperPool is Ownable, Blockable, Callable, PoolInterface {
     sacToken = SacTokenInterface(_sacTokenAddress);
   }
 
+  function getEra(uint256 era) public view returns (Era memory) {
+    return eras[era];
+  }
+
   function approve(
     address delegate,
     uint256 level,
     uint256 currentEra
-  ) public override mustBeAllowedCaller mustBeAbleToApprove(currentEra) {
+  ) public override mustBeAllowedCaller {
+    require(canApprove(currentEra), "You can't approve yet");
+
     uint256 devTokens = tokens(level, currentEra);
 
     sacToken.approveWith(delegate, devTokens);
   }
 
-  function tokens(uint256 level, uint256 currentEra)
-    internal
-    view
-    returns (uint256)
-  {
+  function tokens(uint256 level, uint256 currentEra) internal view returns (uint256) {
     uint256 levels = eras[currentEra].levels;
-    if(levels == 0) return 0;
+    if (levels == 0) return 0;
 
     return level.mul((TOKENS_PER_ERA.div(levels)));
   }
@@ -70,11 +72,11 @@ contract DeveloperPool is Ownable, Blockable, Callable, PoolInterface {
     return balanceOf(address(this));
   }
 
-  function addLevel(uint256 fromEra) public {
+  function addLevel(uint256 fromEra) public mustBeAllowedCaller {
     upLevels(fromEra);
   }
 
-  function removeLevel(uint256 fromEra, uint256 levels) public {
+  function removeLevel(uint256 fromEra, uint256 levels) public mustBeAllowedCaller {
     downLevels(fromEra, levels);
   }
 
@@ -85,13 +87,10 @@ contract DeveloperPool is Ownable, Blockable, Callable, PoolInterface {
   }
 
   function downLevels(uint256 fromEra, uint256 levels) internal {
+    require(eras[fromEra].levels >= levels, "Not enough levels to remove");
+
     for (uint256 i = fromEra; i <= ERAS; i++) {
       eras[i].levels -= levels;
     }
-  }
-
-  modifier mustBeAbleToApprove(uint256 currentEra) {
-    require(canApprove(currentEra), "You can't withdraw yet");
-    _;
   }
 }
