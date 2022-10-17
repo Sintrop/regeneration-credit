f82d9a108359f6daca3a7e95414f2e1d304b15a7
rav-andre <aoravagnani@gmail.com>
2022-10-17 12:01:59 -0300

    add changelog and update migrations

--- migrations/14_deploy_category_contract.js
+++ migrations/14_deploy_category_contract.js
@@ -1,12 +1,14 @@
 const CategoryContract = artifacts.require("CategoryContract");
 const IsaPool = artifacts.require("IsaPool");
 const ResearcherContract = artifacts.require("ResearcherContract");
+const UserContract = artifacts.require("UserContract");
 
 module.exports = function (deployer) {
   deployer.then(async () => {
     const isaPool = await IsaPool.deployed();
     const researcherContract = await ResearcherContract.deployed();
+    const userContract = await UserContract.deployed();
 
-    await deployer.deploy(CategoryContract, isaPool.address, researcherContract.address);
+    await deployer.deploy(CategoryContract, isaPool.address, researcherContract.address, userContract.address);
   });
 };
