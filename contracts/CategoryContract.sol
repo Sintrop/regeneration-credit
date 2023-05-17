863c2bf9db7ed8db6641500cced4d30c2eda2710
Everson Silva <eversonsilva9799@gmail.com>
2023-05-17 20:53:51 -0300

    styles

--- contracts/CategoryContract.sol
+++ contracts/CategoryContract.sol
@@ -96,14 +96,7 @@ contract CategoryContract {
    * @param id the id of a category that receives a vote.
    * @param tokens the tokens amount that the use want use to vote.
    */
-  function vote(
-    uint256 id,
-    uint256 tokens
-  )
-    public
-    requireUserExists
-    categoryMustExists(id)
-  {
+  function vote(uint256 id, uint256 tokens) public requireUserExists categoryMustExists(id) {
     require(isaPool.balanceOf(msg.sender) > tokens, "You don't have tokens to vote");
     require(tokens > 0, "Send at least 1 SAC Token");
     require(voted[msg.sender][id] + tokens <= LIMIT_VOTING, "can't vote more than 100k tokens");
