47c0864a9b7b8129447401a8eaed7d8848119654
Everson Silva <eversonsilva9799@gmail.com>
2023-08-15 08:26:24 -0300

    configure migrations

--- migrations/7_1_deploy_researcher_contract.js
+++ migrations/7_1_deploy_researcher_contract.js
@@ -1,10 +1,12 @@
 const UserContract = artifacts.require("UserContract");
 const ResearcherContract = artifacts.require("ResearcherContract");
+const ResearcherPool = artifacts.require("ResearcherPool");
 
 module.exports = function (deployer) {
   deployer.then(async () => {
     const userContract = await UserContract.deployed();
+    const researcherPool = await ResearcherPool.deployed();
 
-    await deployer.deploy(ResearcherContract, userContract.address);
+    await deployer.deploy(ResearcherContract, userContract.address, researcherPool.address);
   });
 };
