c09903464b26fad34ba79e120f883ccc23136fc9
rav-andre <aoravagnani@gmail.com>
2023-10-03 14:34:01 -0300

    fix text

--- test/RcToken.test.js
+++ test/RcToken.test.js
@@ -54,12 +54,12 @@ contract("RcToken", (accounts) => {
       assert.equal(totalLocked, 0);
     });
 
-    it("balance of contract owner should be equal to 1490000000000000000000000000", async () => {
+    it("balance of contract owner should be equal to 1365000000000000000000000000", async () => {
       const ownerBalance = await instance.balanceOf(ownerAddress);
       assert.equal(ownerBalance, 1365000000000000000000000000n);
     });
 
-    it("balance of rcTokenIco should be 10000000000000000000000000", async () => {
+    it("balance of rcTokenIco should be 135000000000000000000000000", async () => {
       const balance = await instance.balanceOf(rcTokenIco.address);
       assert.equal(balance, 135000000000000000000000000n);
     });
