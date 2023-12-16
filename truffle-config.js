0d910f15ae7c172bccbd308cb4d549d1b656dd17
Everson Silva <eversonsilva9799@gmail.com>
2023-12-16 14:53:30 -0300

    config sepolia network

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,9 @@
-require("dotenv").config({path: __dirname + "/.env"});
+require("dotenv").config({ path: __dirname + "/.env" });
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 
-const privateKey = process.env["PRIVATE_KEY_ACCOUNT_TO_DEPLOY_GOERLI"];
+const privateKey = process.env["PRIVATE_KEY_ACCOUNT_TO_DEPLOY"];
 const infuraKey = process.env["INFURA_API_KEY"];
+const etherscanApiKey = process.env["ETHERSCAN_API_KEY"];
 
 module.exports = {
   networks: {
@@ -11,12 +12,20 @@ module.exports = {
       port: 8545,
       network_id: "*",
     },
-
     goerli: {
       provider: () => new HDWalletProvider(privateKey, `https://goerli.infura.io/v3/${infuraKey}`),
       network_id: "5",
       gas: 4465030,
       gasPrice: 10000000000,
+      skipDryRun: true,
+    },
+    sepolia: {
+      provider: () => new HDWalletProvider(privateKey, `https://sepolia.infura.io/v3/${infuraKey}`),
+      network_id: 11155111,
+      gas: 4000000,
+      gasPrice: 10000000000,
+      confirmations: 2,
+      timeoutBlocks: 15000,
       skipDryRun: true,
     },
   },
@@ -40,4 +49,8 @@ module.exports = {
       },
     },
   },
+  api_keys: {
+    etherscan: etherscanApiKey,
+  },
+  plugins: ["truffle-plugin-verify"],
 };
