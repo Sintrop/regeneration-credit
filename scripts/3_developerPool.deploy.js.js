b266ecf41986fad4c29fd2b1ff78747890243f54
Everson Silva <eversonsilva9799@gmail.com>
2024-02-17 08:29:26 -0300

    update shared service to return instance of deployed contract

--- scripts/3_developerPool.deploy.js.js
+++ scripts/3_developerPool.deploy.js.js
@@ -1,18 +1,21 @@
 const saveContractAddress = require("./shared/saveContractAddress");
-const contractAddress = require("./shared/readContractAddress");
+const getDeployedContract = require("./shared/getDeployedContract");
 
 async function developerPoolDeploy() {
   const pool_halving = process.env["DEVELOPER_POOL_HALVING"];
   const pool_total_eras = process.env["DEVELOPER_POOL_TOTAL_ERAS"];
   const pool_blocks_per_era = process.env["DEVELOPER_POOL_BLOCKS_PER_ERA"];
+  const developerPoolFunds = process.env["DEVELOPER_POOL_FUNDS"];
 
-  const rcTokenAddress = contractAddress("RcToken");
+  const rcToken = await getDeployedContract("RcToken");
 
   const DeveloperPool = await ethers.getContractFactory("DeveloperPool");
 
-  const developerPool = await DeveloperPool.deploy(rcTokenAddress, pool_halving, pool_total_eras, pool_blocks_per_era);
+  const developerPool = await DeveloperPool.deploy(rcToken.target, pool_halving, pool_total_eras, pool_blocks_per_era);
 
   saveContractAddress("DeveloperPool", developerPool.target);
+
+  await rcToken.addContractPool(developerPool.target, developerPoolFunds);
 
   return developerPool;
 }
---
+++ scripts/shared/getDeployedContract.js
@@ -0,0 +1,19 @@
+const fs = require("node:fs");
+
+async function getDeployedContract(contractName) {
+  const contractNameLower = contractName.toLowerCase();
+  const contractsDir = `/app/scripts/deployedContracts`;
+  const filepath = `${contractsDir}/${contractNameLower}.json`;
+
+  const data = fs.readFileSync(filepath, "utf8");
+  object = JSON.parse(data);
+  let contractAddress = object[contractNameLower];
+
+  const Contract = await ethers.getContractFactory(contractName);
+
+  const instance = Contract.attach(contractAddress);
+
+  return instance;
+}
+
+module.exports = getDeployedContract;
