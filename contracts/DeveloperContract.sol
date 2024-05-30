5fd66bec0e0449b571dcd89edbd1d4a812fc72a4
Everson Silva <eversonsilva9799@gmail.com>
2024-05-29 21:57:01 -0300

    add contributionb validations

--- contracts/DeveloperContract.sol
+++ contracts/DeveloperContract.sol
@@ -2,28 +2,43 @@
 pragma solidity >=0.7.0 <=0.9.0;
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { Callable } from "./Callable.sol";
 import { UserContract } from "./UserContract.sol";
 import { UserType } from "./types/UserTypes.sol";
 import { DeveloperPool } from "./DeveloperPool.sol";
-import { Developer, Pool, Contribution } from "./types/DeveloperTypes.sol";
+import { ValidatorContract } from "./ValidatorContract.sol";
+import { Developer, Pool, Contribution, Penalty } from "./types/DeveloperTypes.sol";
 
 /**
  * @title DeveloperContract
  * @dev Developer resource that represent dev
  */
-contract DeveloperContract is Ownable {
+contract DeveloperContract is Ownable, Callable {
   mapping(address => Developer) public developers;
-  mapping(uint256 => mapping(address => Contribution)) public contributions;
+  mapping(uint256 => mapping(address => uint256)) public developerContributionsEra;
+  mapping(uint256 => Contribution) public contributions;
 
+  mapping(address => Penalty[]) public penalties;
   UserContract internal userContract;
   DeveloperPool internal developerPool;
+  ValidatorContract internal validatorContract;
 
   address[] internal developersAddress;
   uint256 public developersCount;
+  uint256 public contributionsCount;
 
-  constructor(address userContractAddress, address developerPoolAddress) {
+  uint256 public immutable MAX_PENALTIES;
+
+  constructor(
+    address userContractAddress,
+    address developerPoolAddress,
+    address validatorContractAddress,
+    uint256 maxPenalties_
+  ) {
     userContract = UserContract(userContractAddress);
     developerPool = DeveloperPool(developerPoolAddress);
+    validatorContract = ValidatorContract(validatorContractAddress);
+    MAX_PENALTIES = maxPenalties_;
   }
 
   /**
@@ -42,6 +57,7 @@ contract DeveloperContract is Ownable {
       name,
       proofPhoto,
       Pool(level, poolEra),
+      0,
       block.number
     );
 
@@ -53,20 +69,72 @@ contract DeveloperContract is Ownable {
 
   function addContribution(string memory report) public {
     uint256 currentEra = developerPoolEra();
-    Contribution memory contribution = contributions[currentEra][msg.sender];
 
     require(userContract.userTypeIs(UserType.DEVELOPER, msg.sender), "Only Developer");
-    require(!contribution.contributed, "Already has contribution");
 
-    contributions[developerPoolEra()][msg.sender] = Contribution(
+    uint256 contributionEra = developerContributionsEra[currentEra][msg.sender];
+
+    require(contributionEra > 0, "Already has contribution");
+
+    developerContributionsEra[currentEra][msg.sender] = 1;
+
+    contributionsCount++;
+    uint256 id = contributionsCount;
+
+    developers[msg.sender].totalContributions++;
+
+    contributions[id] = Contribution(
+      id,
       currentEra,
+      msg.sender,
       developers[msg.sender].pool.level,
       report,
+      0,
       true,
+      true,
+      0,
+      currentEra,
       block.number
     );
 
     updateLevel(msg.sender);
+  }
+
+  function addContributionValidation(uint256 id, string memory justification) public {
+    require(userContract.userTypeIs(UserType.VALIDATOR, msg.sender), "Please register as validator");
+
+    Contribution memory contribution = contributions[id];
+
+    require(contribution.valid && contribution.createdInEra == developerPoolEra(), "This contribution is not VALID");
+
+    contribution.validationsCount += 1;
+    contributions[id] = contribution;
+
+    bool mustInvalidateInspection = contribution.validationsCount >= validatorContract.majorityValidatorsCount();
+
+    if (mustInvalidateInspection) invalidateContribution(contribution);
+
+    validatorContract.addDeveloperContributionValidation(contribution, justification, msg.sender);
+  }
+
+  function invalidateContribution(Contribution memory contribution) internal {
+    contribution.valid = false;
+    contribution.invalidatedAt = block.number;
+    contributions[contribution.id] = contribution;
+  }
+
+  function resetLevels(address addr, uint256 removeSomeLevels) public mustBeAllowedCaller {
+    Developer memory developer = developers[addr];
+    
+    developers[addr].pool.level -= removeSomeLevels > 0 ? removeSomeLevels : developer.pool.level;
+
+    developerPool.resetLevels(addr, developer.pool.currentEra, removeSomeLevels);
+  }
+
+  function decrementContributions(address addr) public mustBeAllowedCaller {
+    require(developers[addr].totalContributions > 0, "totalInspections invalid");
+
+    developers[addr].totalContributions--;
   }
 
   /**
@@ -121,6 +189,16 @@ contract DeveloperContract is Ownable {
     developers[addr] = developer;
 
     developerPool.addLevel(addr, developer.pool.level, 1);
+  }
+
+  function addPenalty(address addr, uint256 contributionId) public mustBeAllowedCaller returns (uint256) {
+    penalties[addr].push(Penalty(contributionId));
+
+    return totalPenalties(addr);
+  }
+
+  function totalPenalties(address addr) public view returns (uint256) {
+    return penalties[addr].length;
   }
 
   /**
