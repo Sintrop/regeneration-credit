3e733c7afde679d7b4bd32c7fcea490dc8e054fd
rav-andre <aoravagnani@gmail.com>
2025-04-03 19:23:01 -0300

    new comments

--- contracts/types/SupporterTypes.sol
+++ contracts/types/SupporterTypes.sol
@@ -3,6 +3,13 @@ pragma solidity >=0.7.0 <=0.9.0;
 
 import "./CommunityTypes.sol";
 
+/**
+ * @dev Supporter user type data structure
+ * @param id User id
+ * @param supporterWallet Supporter wallet address
+ * @param name User name
+ * @param createdAt Block of user creation
+ */
 struct Supporter {
   uint256 id;
   address supporterWallet;
@@ -10,6 +17,14 @@ struct Supporter {
   uint256 createdAt;
 }
 
+/**
+ * @dev Publication data structure
+ * @param supporterAddress Supporter wallet address
+ * @param createdAt Block of creation
+ * @param amount Tokens burned
+ * @param description Publication description
+ * @param content Publication content
+ */
 struct Publication {
   address supporterAddress;
   uint256 createdAt;
