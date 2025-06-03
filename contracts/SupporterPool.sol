17535a2115d590631e050fb6547c3a96be38c33c
rav-andre <aoravagnani@gmail.com>
2025-06-03 11:52:04 -0300

    update solidity version

--- contracts/SupporterPool.sol
+++ contracts/SupporterPool.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.7.0 <=0.9.0;
+pragma solidity >=0.8.0 <0.9.0;
 
 import { Callable } from "./shared/Callable.sol";
 import { RegenerationCredit } from "./RegenerationCredit.sol";
