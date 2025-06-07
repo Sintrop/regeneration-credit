cd1fd8d82c92b3439a8fff1f804fbc822ee34f1f
rav-andre <aoravagnani@gmail.com>
2025-06-07 18:00:58 -0300

    update pools comments

--- contracts/DeveloperPool.sol
+++ contracts/DeveloperPool.sol
@@ -9,19 +9,34 @@ import { Callable } from "./shared/Callable.sol";
 import { Poolable } from "./shared/Poolable.sol";
 
 /**
- * @author Sintrop
  * @title DeveloperPool
- * @dev Manage rewards to developers
- * @notice Receive tokens for development service provided
+ * @author Sintrop
+ * @notice This contract manages the distribution of Regeneration Credit tokens as rewards to developers
+ * for their code development, primarily for the development of the system software.
+ * Developers can contribute to the 'core' front-end, to smart-contracts, UX, website, evaluation methods and any other related open-source projects.
+ * Each valid published development report is equivalent to one level in the pool.
+ * @dev Inherits core functionalities from `Poolable` (for pool management), `Ownable` (for ownership),
+ * `Blockable` (for era/epoch tracking), and `Callable` (for whitelisted caller control).
  */
 contract DeveloperPool is Poolable, Ownable, Blockable, Callable {
   using SafeMath for uint256;
 
+  /// @notice Interface to the Regeneration Credit token contract, used for token transfers.
   RegenerationCreditInterface internal regenerationCredit;
 
-  /// @notice Total developer pool tokens
+  /// @notice The total supply of Regeneration Credit tokens designated for this developer pool.
+  /// This value represents the maximum tokens available for distribution through this contract.
   uint256 internal constant TOTAL_POOL_TOKENS = 40000000000000000000000000;
 
+  /**
+   * @dev Initializes the DeveloperPool contract.
+   * Sets up the Regeneration Credit token interface and initializes inherited base contracts.
+   * @param regenerationCreditAddress The address of the RegenerationCredit token contract.
+   * @param _halving The number of eras that constitute one halving cycle/epoch for reward adjustments.
+   * Passed to the `Blockable` base contract.
+   * @param _blocksPerEra The number of blocks that constitute a single era.
+   * Passed to the `Blockable` base contract.
+   */
   constructor(
     address regenerationCreditAddress,
     uint256 _halving,
@@ -30,38 +45,53 @@ contract DeveloperPool is Poolable, Ownable, Blockable, Callable {
     regenerationCredit = RegenerationCreditInterface(regenerationCreditAddress);
   }
 
+  // --- Public Functions ---
+
   /**
-   * @dev Called by the developer contract, this function calls the token contract to transfer the rewards
-   * @param delegate User address
-   * @param era User current era
+   * @dev Allows an authorized caller, the Developer contract, to trigger a token withdrawal for a user.
+   * This function calculates the eligible tokens for the user's era and transfers them.
+   * @notice This function can only be called by the DeveloperRules contract, whitelisted via the `Callable` contract's mechanisms.
+   * The user must also be eligible for withdrawal based on the `Blockable` contract's era tracking.
+   * @param delegate The address of the user (developer) for whom the withdrawal is being processed.
+   * @param era The last recorded era of the `delegate` user, used for reward calculation and eligibility.
    */
   function withdraw(address delegate, uint256 era) public mustBeAllowedCaller canWithdrawModifier(era) {
+    require(era <= currentContractEra(), "Era in the future");
+
+    // Calculate the number of tokens the user is eligible to receive for the given era.
     uint256 numTokens = calculateUserEraTokens(era, delegate, tokensPerEra(getEpochForEra(era), HALVING));
 
+    // Update the user's era and token balance state after the withdrawal.
     updateEraAfterWithdraw(era, delegate, numTokens);
 
+    // If no tokens are to be transferred, return.
     if (numTokens == 0) return;
 
+    // Transfer the calculated tokens from this contract to the delegate.
     regenerationCredit.transferWith(address(this), delegate, numTokens);
   }
 
   /**
-   * @dev Called by the developer contract, function to increase developer pool level
-   * @param addr Developer wallet
-   * @param levels Levels to increase
+   * @dev Allows an authorized caller to increase the user pool level.
+   * This function updates the developer level within the system's pooling mechanism.
+   * @notice Can only be called by the developerRules address.
+   * @param addr The wallet address of the developer.
+   * @param levels The number of levels to increase the developer's pool level by.
    */
   function addLevel(address addr, uint256 levels) public mustBeAllowedCaller {
-    uint256 era = currentContractEra();
-
-    addPoolLevel(addr, levels, era);
+    // Calls the addPoolLevel function from Poolable.sol.
+    addPoolLevel(addr, levels, currentContractEra());
   }
 
   /**
-   * @dev Called by the developer contract, function to decrease developer pool level
-   * @param addr Developer wallet
-   * @param levelsToRemove Levels to remove
+   * @dev Allows an authorized caller to decrease an developer's pool level.
+   * This function adjusts the developer's level downwards within the system's pooling mechanism.
+   * @notice Can only be called by developerRules address.
+   * @param addr The wallet address of the developer.
+   * @param levelsToRemove The number of levels to decrease the developer's pool level by.
    */
   function removePoolLevels(address addr, uint256 levelsToRemove) public mustBeAllowedCaller {
+    // Calls the removePoolLevel function from Poolable.sol.
     removePoolLevel(addr, currentContractEra(), levelsToRemove);
   }
 }
