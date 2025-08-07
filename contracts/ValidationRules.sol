ef54ec937e7c647a850d49fdc17e8791b6f5e714
rav-andre <aoravagnani@gmail.com>
2025-08-07 19:33:54 -0300

    run prettier

--- contracts/ValidationRules.sol
+++ contracts/ValidationRules.sol
@@ -81,17 +81,17 @@ contract ValidationRules is Callable, ReentrancyGuard {
   /// @notice Relationship between validator and last vote block.number.
   mapping(address => uint256) public validatorLastVoteAt;
 
-/// @notice Tracks inspection IDs that have already been processed.
-mapping(uint256 => bool) public inspectionAlreadyInvalidated;
+  /// @notice Tracks inspection IDs that have already been processed.
+  mapping(uint256 => bool) public inspectionAlreadyInvalidated;
 
-/// @notice Tracks report IDs that have already been processed.
-mapping(uint256 => bool) public reportAlreadyInvalidated;
+  /// @notice Tracks report IDs that have already been processed.
+  mapping(uint256 => bool) public reportAlreadyInvalidated;
 
-/// @notice Tracks research IDs that have already been processed.
-mapping(uint256 => bool) public researchAlreadyInvalidated;
+  /// @notice Tracks research IDs that have already been processed.
+  mapping(uint256 => bool) public researchAlreadyInvalidated;
 
-/// @notice Tracks contribution IDs that have already been processed.
-mapping(uint256 => bool) public contributionAlreadyInvalidated;
+  /// @notice Tracks contribution IDs that have already been processed.
+  mapping(uint256 => bool) public contributionAlreadyInvalidated;
 
   /// @notice CommunityRules contract interface.
   ICommunityRules private communityRules;
@@ -255,8 +255,8 @@ mapping(uint256 => bool) public contributionAlreadyInvalidated;
 
     require(!inspectionAlreadyInvalidated[inspection.id], "Penalties already applied");
 
-    inspectionAlreadyInvalidated[inspection.id] = true;    
-    
+    inspectionAlreadyInvalidated[inspection.id] = true;
+
     uint256 inspectorTotalPenalties = inspectorRules.addPenalty(inspection.inspector, inspection.id);
     _removeUserInspection(inspection);
 
@@ -334,9 +334,9 @@ mapping(uint256 => bool) public contributionAlreadyInvalidated;
 
     require(!contributionAlreadyInvalidated[contribution.id], "Penalties already applied");
 
-    contributionAlreadyInvalidated[contribution.id] = true; 
+    contributionAlreadyInvalidated[contribution.id] = true;
 
-    uint256 contributorTotalPenalties = contributorRules.addPenalty(contribution.user, contribution.id); 
+    uint256 contributorTotalPenalties = contributorRules.addPenalty(contribution.user, contribution.id);
 
     _removeContribution(contribution);
 
@@ -374,7 +374,7 @@ mapping(uint256 => bool) public contributionAlreadyInvalidated;
 
     require(!researchAlreadyInvalidated[research.id], "Penalties already applied");
 
-    researchAlreadyInvalidated[research.id] = true;  
+    researchAlreadyInvalidated[research.id] = true;
 
     uint256 totalPenalties = researcherRules.addPenalty(research.createdBy, research.id);
 
